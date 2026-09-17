dd6ad760dfcd2a76029f07934969c6f530973b61
Pasha <p@past5578.dev>
2026-09-17 12:37:14 -0500

    Add file_path key

--- migrations/01-create-image_metadata.sql
+++ migrations/01-create-image_metadata.sql
@@ -2,6 +2,7 @@ CREATE TABLE
     IF NOT EXISTS image_metadata (
         id uuid PRIMARY KEY,
         extension text NOT NULL,
+        file_path text NOT NULL,
         width integer NOT NULL,
         height integer NOT NULL,
         created_at timestamptz NOT NULL
