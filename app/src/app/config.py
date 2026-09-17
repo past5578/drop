d35365a3e8c210750ddfdf87cf5402b2378d4e05
Pasha <p@past5578.dev>
2026-09-17 09:25:43 -0500

    Changed to use exist_ok for mkdir

--- app/src/app/config.py
+++ app/src/app/config.py
@@ -13,9 +13,7 @@ DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
 
 def _storage_path():
     storage_path = Path(os.environ["STORAGE_PATH"])
-
-    if not storage_path.exists():
-        storage_path.mkdir()
+    storage_path.mkdir(exist_ok=True)
 
     if not IS_DOCKER:
         (storage_path / ".gitignore").write_text("*")
@@ -24,5 +22,9 @@ def _storage_path():
 
 
 STORAGE_PATH = _storage_path()
+
 IMAGE_PATH = STORAGE_PATH / "images"
+IMAGE_PATH.mkdir(exist_ok=True)
+
 THUMB_PATH = STORAGE_PATH / "thumbs"
+THUMB_PATH.mkdir(exist_ok=True)
