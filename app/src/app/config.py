6ec4f4f20244d0d2290b0633752551c90d0fbb9d
Pasha <p@past5578.dev>
2026-09-17 12:36:43 -0500

    Add APP_URL in config (should've been previous commit)

--- app/src/app/config.py
+++ app/src/app/config.py
@@ -7,6 +7,8 @@ load_dotenv()
 
 IS_DOCKER = bool(os.environ.get("DOCKER"))
 
+APP_URL = os.environ["APP_URL"]
+
 DATABASE_URL = os.environ["DATABASE_URL"]
 DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
 
